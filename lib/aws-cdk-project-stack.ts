741bc2b69eb27b4e44ccfe94486dbc5fcbb11727
maxwangsongyuan <songyuanwangcode@gmail.com>
2024-08-10 21:25:21 -0700

    Fix s3 duplicated name issue to ensure uniqueness

--- lib/aws-cdk-project-stack.ts
+++ lib/aws-cdk-project-stack.ts
@@ -6,16 +6,25 @@ export class AwsCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define the S3 bucket
+    // Create a unique bucket name using account ID, region, and date-time
+    const account = cdk.Stack.of(this).account;
+    const region = cdk.Stack.of(this).region;
+
+    // Generate the current date and time
+    const now = new Date();
+    const dateString = now.toISOString().replace(/[:\-]/g, '').replace(/\..+/, '');
+
+    // Define the S3 bucket with a unique name
     new s3.Bucket(this, 'MyBucket', {
-      bucketName: 'my-unique-bucket-name-7864899851256',
+      bucketName: `my-unique-bucket-${account}-${region}-${dateString}`,
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete the bucket when the stack is deleted
       autoDeleteObjects: true, // Automatically delete all objects in the bucket when the stack is deleted
     });
 
+    // Define another S3 bucket with a unique name
     new s3.Bucket(this, 'MyBucket2', {
-      bucketName: 'test-github-action-71sdadsad4a54das',
+      bucketName: `test-github-action-${account}-${region}-${dateString}`,
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete the bucket when the stack is deleted
       autoDeleteObjects: true, // Automatically delete all objects in the bucket when the stack is deleted
