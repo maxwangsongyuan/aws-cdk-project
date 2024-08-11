624ed88680915d520a0787b36cbefcae4a045011
maxwangsongyuan <songyuanwangcode@gmail.com>
2024-08-10 19:35:47 -0700

    create s3 bucket

--- lib/aws-cdk-project-stack.ts
+++ lib/aws-cdk-project-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class AwsCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the S3 bucket
+    new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'my-unique-bucket-name-864899851256',
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete the bucket when the stack is deleted
+      autoDeleteObjects: true, // Automatically delete all objects in the bucket when the stack is deleted
+    });
   }
 }
